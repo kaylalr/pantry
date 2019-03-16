2f43053d6e5db0d9128f409c006fa5442b4db256
Kayla Roberts <klr3of8@gmail.com>
2019-03-16 11:49:57 -0600

    edit button works

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(express.static('bootstrap'));
 app.use(express.static('public'));
 /* these two allow you to get data from
-*  the body on a post     */
+ *  the body on a post     */
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -62,16 +62,10 @@ app.post("/login", function (req, res) {
 app.get("/food", function (req, res) {
     // ??? is this the best way to do this?  look at the getAllFoods function
     getAllFoods(function (result) {
-        // console.log("food DB result: ")
-        // console.log(result);
         const params = {
             foods: result
         };
         console.log(params);
-        // for(i=0; i<result.length; i++) {
-        // console.log("result " + i + ":");
-        // console.log(result[i]);
-        // }
         res.render("food", params);
     })
 });
@@ -87,7 +81,7 @@ app.get("/addFood", function (req, res) {
             console.log(params);
             res.render("addFood", params);
         })
-        
+
     })
 })
 app.post("/addFood", function (req, res) {
@@ -98,7 +92,6 @@ app.post("/addFood", function (req, res) {
     let expiration = req.body.expiration;
     let description = req.body.description;
 
-    // console.log("food_name: "+food_name+", food_type: "+food_type+", quantity_num: "+quantity_num+", quantity_type: "+quantity_type+", expiration: "+expiration+", description: "+description);
     var insertSQL = "INSERT INTO foods values (default,$1,1,$2,$3,$4,$5,$6)";
     pool.query(insertSQL, [food_name, food_type, quantity_num, quantity_type, expiration, description], function (err, result) {
         // If an error occurred...
@@ -107,15 +100,58 @@ app.post("/addFood", function (req, res) {
             console.log(err);
         }
     })
+});
+app.get("/edit/:id", function (req, res) {
+    let id = req.params.id;
+    getQuantityTypes(function (result1) {
+        getFoodGroups(function (result2) {
+            getFoodById(id, function (result3) {
+                const params = {
+                    food: result3[0],
+                    quantity_types: result1,
+                    food_groups: result2
+                };
+                console.log(params);
+                res.render("editFood", params);
+            })
+        })
+    })
+})
+app.post("/edit/:id", function (req, res) {
+    let id = req.params.id;
+    // WRITE FUNCTION TO UPDATE DATABASE
 })
 
 app.get("/bootstrap", function (req, res) {
     res.render("index-new");
 })
 
+function getFoodById(id, callback) {
+    var getFoodById = "SELECT * FROM foods WHERE food_id = $1";
+    pool.query(getFoodById, [id], function (err, result) {
+        if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+        }
+        let year = result.rows[0].expiration.getFullYear();
+        let month = result.rows[0].expiration.getMonth();
+        console.log("month length: " + month.length);
+        if (month.toString().length == 1) {
+            month = "0"+month;
+        }
+        let day = result.rows[0].expiration.getDate();
+        if (day.toString().length == 1) {
+            day = "0"+day;
+        }
+        let date = year + "-" + month + "-" + day;
+        result.rows[0].expiration = date;
+        callback(result.rows);
+    })
+}
+
 function getAllFoods(callback) {
     // var getAllFoods = "SELECT * FROM foods";
-    var getAllFoods = "select f.food_name, f.foodgroup_id, f.quantity_num, f.expiration, qt.quantity_type_name FROM foods f JOIN quantity_types qt ON qt.quantity_type_id = f.quantity_type_id;"
+    var getAllFoods = "select f.food_id, f.food_name, f.foodgroup_id, f.quantity_num, f.expiration, qt.quantity_type_name FROM foods f JOIN quantity_types qt ON qt.quantity_type_id = f.quantity_type_id;"
     pool.query(getAllFoods, function (err, result) {
         if (err) {
             console.log("Error in query: ")
@@ -146,27 +182,6 @@ function getFoodGroups(callback) {
         callback(result.rows);
     })
 }
-
-// app.get("/getPerson", function (req, res) {
-//     let inputs = url.parse(req.url, true);
-//     let personId = inputs.query.id;
-
-//     var getPersonById = "SELECT * FROM person WHERE id = $1";
-
-//     pool.query(getPersonById, [personId], function (err, result) {
-//         // If an error occurred...
-//         if (err) {
-//             console.log("Error in query: ")
-//             console.log(err);
-//         }
-
-//         // Log this to the console for debugging purposes.
-//         console.log("Back from DB with result:");
-//         console.log(result.rows);
-
-//         res.json(result.rows);
-//     })
-// });
 
 app.listen(PORT, function () {
     console.log("Server is running...");
