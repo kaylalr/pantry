34063dfcf955a406318758b38a7a254d1dccd3ea
Kayla Roberts <klr3of8@gmail.com>
2019-03-20 12:17:01 -0600

    login test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const url = require('url');
 const PORT = process.env.PORT || 5000;
 const session = require('express-session');
-let stayLoggedin = true;
+let stayLoggedin = false;
 // const bodyParser = require('body-parser');
 
 const {
